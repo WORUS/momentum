0c811833c7830c90a2dd0ea1563d4ca2234147fc
WORUS <aswoks@gmail.com>
2023-07-05 12:43:45 +0400

    feat: add background

--- index.js
+++ index.js
@@ -16,13 +16,20 @@ const setUserName = () => {
 const handleKeyDown = (event) => {
     if(event.code === "Enter"){         //=== сравнение типа данных
         localStorage.setItem("username", event.currentTarget.value);
-
     }
-    
 }
+const changeBackgroundImage = () => {
+    const body = document.querySelector("body");
+    setInterval(() => {
+        let rndImg = Math.floor(Math.random()*3 + 1);
+        body.style.background = `url("../Images/${rndImg}.jpg")`;
+    }, 10000);
+}
+
 
 showTime();
 setUserName();
+changeBackgroundImage();
 
 const input = document.querySelector(".user-name");
 input.onkeydown = handleKeyDown;
